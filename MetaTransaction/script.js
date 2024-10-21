061095e1974daa9a03c7a1ddbadf121a0b0a3d9a
uplusproject <uplusproject@gmail.com>
2024-10-21 16:43:31 +0800

    Update script.js

--- MetaTransaction/script.js
+++ MetaTransaction/script.js
@@ -1,13 +1,40 @@
 let web3;
 let userAddress;
 
+// 合约地址
+const contractAddress = '0x56E7Ab18FA30C4D7887914f1113272Ca22a63aED'; // 替换为您的合约地址
+
+// 合约 ABI
+const abi = [
+    {
+        "inputs": [
+            { "internalType": "address", "name": "recipient", "type": "address" },
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "executeETHTransfer",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "tokenAddress", "type": "address" },
+            { "internalType": "address", "name": "recipient", "type": "address" },
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "executeTokenTransfer",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
 document.getElementById('connectButton').onclick = async () => {
     if (window.ethereum) {
         try {
             web3 = new Web3(window.ethereum);
             await window.ethereum.request({ method: 'eth_requestAccounts' });
             userAddress = (await web3.eth.getAccounts())[0];
-
             alert('钱包连接成功: ' + userAddress);
             document.getElementById('transferForm').style.display = 'block'; // 显示转移表单
         } catch (error) {
@@ -29,19 +56,7 @@ document.getElementById('ethTransferForm').onsubmit = async (event) => {
 
     try {
         const signature = await web3.eth.sign(messageHash, userAddress);
-        const contractAddress = '0x56E7Ab18FA30C4D7887914f1113272Ca22a63aED'; // 替换为您的合约地址
-        const contract = new web3.eth.Contract([
-            {
-                "inputs": [
-                    { "internalType": "address", "name": "recipient", "type": "address" },
-                    { "internalType": "uint256", "name": "amount", "type": "uint256" }
-                ],
-                "name": "executeETHTransfer",
-                "outputs": [],
-                "stateMutability": "nonpayable",
-                "type": "function"
-            }
-        ], contractAddress);
+        const contract = new web3.eth.Contract(abi, contractAddress);
 
         await contract.methods.executeETHTransfer(recipient, amountInWei, signature).send({ from: userAddress });
         alert('ETH 转移成功！');
@@ -62,20 +77,7 @@ document.getElementById('tokenTransferForm').onsubmit = async (event) => {
 
     try {
         const signature = await web3.eth.sign(messageHash, userAddress);
-        const contractAddress = '0x56E7Ab18FA30C4D7887914f1113272Ca22a63aED'; // 替换为您的合约地址
-        const contract = new web3.eth.Contract([
-            {
-                "inputs": [
-                    { "internalType": "address", "name": "tokenAddress", "type": "address" },
-                    { "internalType": "address", "name": "recipient", "type": "address" },
-                    { "internalType": "uint256", "name": "amount", "type": "uint256" }
-                ],
-                "name": "executeTokenTransfer",
-                "outputs": [],
-                "stateMutability": "nonpayable",
-                "type": "function"
-            }
-        ], contractAddress);
+        const contract = new web3.eth.Contract(abi, contractAddress);
 
         await contract.methods.executeTokenTransfer(tokenAddress, recipient, amount, signature).send({ from: userAddress });
         alert('代币转移成功！');
